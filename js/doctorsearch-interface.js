d52f0ef1a55b211ae32dde66a358838bf0324d6f
Asia Kane <asialkane@gmail.com>
2017-05-26 10:23:38 -0700

    add function to display message if no results are returned

--- js/doctorsearch-interface.js
+++ js/doctorsearch-interface.js
@@ -1,10 +1,22 @@
 var DoctorSearch = require ('./../js/doctorsearch.js').doctorSearchModule;
 
+var displayDoctors = function(result) {
+  $('.list-of-docs').text("");
+  result.data.forEach(function(doctor) {
+    $('.list-of-docs').append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`);
+  });
+};
+
+var displayNoDoctors = function() {
+  $('.list-of-docs').html(`<p>Your search did not return any results.  Consider visiting an urgent care facility in your area by visiting https://www.urgentcarelocations.com/.</p>`);
+};
+
 $(document).ready(function(){
   $('#generate-doctors').click(function() {
     var symptomInput = $('#symptom-input').val();
+    $('#symptom-input').val("");
     var newDoctorSearch = new DoctorSearch();
-    newDoctorSearch.searchBySymptom(symptomInput);
+    newDoctorSearch.searchBySymptom(symptomInput, displayDoctors, displayNoDoctors);
 
   });
 }); // Document Ready Close
--- js/doctorsearch.js
+++ js/doctorsearch.js
@@ -3,10 +3,14 @@ var apiKey = require('./../.env').apiKey;
 function DoctorSearch() {
 }
 
-DoctorSearch.prototype.searchBySymptom = function (symptom) {
+DoctorSearch.prototype.searchBySymptom = function (symptom, displayDoctors, displayNoDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptom+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result.data[0].profile.first_name, result.data[0].profile.last_name);
+    .then(function(result) {
+      if (result.data.length <= 0) {
+        displayNoDoctors();
+      } else {
+        displayDoctors(result);
+      }
     })
   .fail(function(error) {
      $('.list-of-docs').text(error.responseJSON.message);
