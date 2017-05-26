9758a2362775ebf39884ef1e0543d071fa053ba8
Asia Kane <asialkane@gmail.com>
2017-05-26 11:42:07 -0700

    include facility and location in results returned from API query

--- js/doctorsearch-interface.js
+++ js/doctorsearch-interface.js
@@ -3,12 +3,17 @@ var DoctorSearch = require ('./../js/doctorsearch.js').doctorSearchModule;
 var displayDoctors = function(result) {
   $('.list-of-docs').text("");
   result.data.forEach(function(doctor) {
-    $('.list-of-docs').append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`);
+    $('.list-of-docs').append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}
+        <ul>
+          <li>Facility:   ${doctor.practices[0].name}</li>
+          <li>Location:   ${doctor.practices[0].visit_address.street},  ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}  ${doctor.practices[0].visit_address.zip}</li>
+        </ul>
+      </li>`);
   });
 };
 
 var displayNoDoctors = function() {
-  $('.list-of-docs').html(`<p>Your search did not return any results.  Consider visiting an urgent care facility in your area by visiting https://www.urgentcarelocations.com/.</p>`);
+  $('.list-of-docs').html(`<p>Your search did not return any results.  Consider visiting an urgent care facility in your area by visiting <a>https://www.urgentcarelocations.com/.</a></p>`);
 };
 
 $(document).ready(function(){
